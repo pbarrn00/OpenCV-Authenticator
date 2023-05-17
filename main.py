376708a019072a4cbefaedec2b751e35229724f9
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2023-05-17 13:47:47 +0200

    Arreglado Bug de guardado de Frames

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Cargar el clasificador frontal de Haar para detección de rostros utilizado en la práctica 4 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Inicializar la cámara
 video = cv2.VideoCapture(0)
@@ -25,15 +25,6 @@ while True:
     for (x, y, w, h) in faces:
         # Dibujar un rectángulo alrededor del rostro
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        # Recortar el rostro de la imagen original
-        face_image = frame[y:y+h, x:x+w]
-
-        # Guardar la imagen del rostro recortado
-        image_count += 1
-        image_filename = f'rostro_{image_count}.png'
-        cv2.imwrite(image_filename, face_image)
-        print(f"Imagen {image_filename} guardada.")
 
     # Mostrar el fotograma con los rectángulos dibujados
     cv2.imshow('Streaming de Video', frame)
