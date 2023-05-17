11f4a01b3643203d432cb0b40afe2d9f5b2679f0
pbarrn00 <pbarrn00@estudiantes.unileon.es>
2023-05-17 21:56:05 +0200

    parte básica acabada

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-import cv2 
+import cv2
 import numpy as np
 import sys
 import os
 
-try:
-    max_images = int(sys.argv[1])  # Número máximo de imágenes que se guardarán
-except IndexError:
-    print('Error: No se ha proporcionado el número máximo de imágenes como parámetro.')
-    sys.exit(1)
-    
-# Creamos una carpeta para almacenar los rostros si esta no existe
-carpeta_rostros = 'rostros'
-
-if not os.path.exists('rostros'):
-    print('Carpeta creada: rostros')
-    os.makedirs(carpeta_rostros, exist_ok=True)
-
-# Cargar el clasificador frontal de Haar para detección de rostros utilizado en la práctica 4 
+# Cargar el clasificador frontal de Haar para detección de rostros utilizado en la práctica 4
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Inicializar la cámara
 video = cv2.VideoCapture(0)
 
-# Variable para contar las imágenes capturadas
+# Definir y cargar usuarios registrados
+usuarios_dict = {}  # Diccionario para mapear nombres de usuarios a etiquetas
+usuarios_imagenes = []
+usuarios_etiquetas = []
+carpeta_usuarios = 'usuarios_registrados'
+
+usuarios = os.listdir(carpeta_usuarios)
+for i, usuario in enumerate(usuarios):
+    usuarios_dict[usuario] = i
+
+    carpeta_usuario = os.path.join(carpeta_usuarios, usuario)
+    if os.path.isdir(carpeta_usuario):
+        for imagen_nombre in os.listdir(carpeta_usuario):
+            imagen_path = os.path.join(carpeta_usuario, imagen_nombre)
+            imagen = cv2.imread(imagen_path, cv2.IMREAD_GRAYSCALE)
+            usuarios_imagenes.append(imagen)
+            usuarios_etiquetas.append(i)
+
+# Crear el modelo de reconocimiento facial
+modelo = cv2.face.LBPHFaceRecognizer_create()
+modelo.train(usuarios_imagenes, np.array(usuarios_etiquetas, dtype=np.int32))
+
+# Variables para contar las imágenes capturadas y el usuario identificado
 image_count = 0
-max_images = int(sys.argv[1])  # Número máximo de imágenes que se guardarán
+usuario_identificado = None
 
 # Bucle principal
 while True:
@@ -44,47 +53,38 @@ while True:
 
     # Iterar sobre los rostros detectados
     for (x, y, w, h) in faces:
-        # Dibujar un rectángulo alrededor del rostro
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         # Recortar el rostro de la imagen original
-        face_image = frame[y:y+h, x:x+w]
+        face_image = gray[y:y+h, x:x+w]
 
-        # Guardar la imagen del rostro recortado
+        # Realizar la predicción del usuario
+        etiqueta_predicha, confianza = modelo.predict(face_image)
 
-        if k == ord('s'):
-            for i in range(0, max_images):
-                image_filename = os.path.join(carpeta_rostros, 'rostro_{}.jpg'.format(image_count))
-                image_count += 1
-                cv2.imwrite(image_filename, face_image)
-                print(f"Imagen {image_filename} guardada.")
-                cv2.imshow('Rostros', face_image)
+        # Buscar el nombre del usuario en base a la etiqueta predicha
+        nombre_usuario = [usuario for usuario, etiqueta in usuarios_dict.items() if etiqueta == etiqueta_predicha][0]
 
+        # Dibujar un rectángulo alrededor del rostro
+        if usuario_identificado == nombre_usuario:
+            color_rectangulo = (0, 255, 0)  # Color verde para usuarios identificados
+        else:
+            color_rectangulo = (0, 0, 255)  # Color rojo para usuarios desconocidos
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color_rectangulo, 2)
+
+        if confianza < 70:  # Umbral de confianza para la identificación del usuario
+            usuario_identificado = nombre_usuario
+            color_rectangulo = (0, 255, 0)  # Color verde para usuarios registrados
+        else:
+            usuario_identificado = "Desconocido"
+            color_rectangulo = (0, 0, 255)  # Color rojo para usuarios no registrados
+
+        # Mostrar el nombre del usuario en la interfaz gráfica
+        cv2.putText(frame, usuario_identificado, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_rectangulo, 1, cv2.LINE_AA)
+        
+    cv2.putText(frame, f'Presione ESC para salir', (10, 20), 2, 0.5, (128, 0, 255), 1, cv2.LINE_AA)
     # Mostrar el fotograma con los rectángulos dibujados
-    #cv2.rectangle(frame, (10, 5), (450, 25), (255, 255, 255), -1)
-
-    # Construir el texto a mostrar
-    text = f'Presione ESC para salir\nPresione S para guardar imagenes.\nImagenes guardadas: {image_count}/{max_images}'
-
-    # Mostrar el texto en varias líneas con fondo blanco
-    lines = text.split('\n')
-    y_pos = 20
-    line_height = 20
-    rectangle_padding = 5
-    for line in lines:
-        # Obtener las dimensiones del texto
-        (w, h), _ = cv2.getTextSize(line, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-
-        # Dibujar el rectángulo de fondo blanco
-        cv2.rectangle(frame, (10, y_pos - h - rectangle_padding), (10 + w + 2*rectangle_padding, y_pos + rectangle_padding), (255, 255, 255), cv2.FILLED)
-
-        # Mostrar el texto
-        cv2.putText(frame, line, (10 + rectangle_padding, y_pos), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 0, 255), 1, cv2.LINE_AA)
-
-        y_pos += line_height    
-
     cv2.imshow('Streaming de Video', frame)
-    
+
 # Liberar los recursos
 video.release()
 cv2.destroyAllWindows()
